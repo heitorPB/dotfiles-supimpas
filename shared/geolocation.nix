6933fad82ac1641bdd3ef9a07a174be33df1fb97
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2023-11-29 22:06:06 -0300

    Move common files to shared/
    Add Helsinki to geolocation and sort alphabetically.

---
+++ shared/geolocation.nix
@@ -0,0 +1,35 @@
+{
+  # https://m.xkcd.com/2170/
+
+  geneve = {
+    city = "Geneve";
+    country = "Switzerland";
+    timezone = "Europe/Zurich";
+    latitude = "60.2";
+    longitude = "24.9";
+  };
+
+  helsinki = {
+    city = "Helsinki";
+    country = "Finland";
+    timezone = "Europe/Finland";
+    latitude = "46.2339717";
+    longitude = "6.0557211";
+  };
+
+  piracicaba = {
+    city = "Piracicaba";
+    country = "Brazil";
+    timezone = "America/Sao_Paulo";
+    latitude = "-22.7";
+    longitude = "-47.6";
+  };
+
+  saoCarlos = {
+    city = "São Carlos";
+    country = "Brazil";
+    timezone = "America/Sao_Paulo";
+    latitude = "-22.0183737";
+    longitude = "-47.8911570";
+  };
+}

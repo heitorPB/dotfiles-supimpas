61939984ba1d0c6822a82a40bd1f5252710727cc
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-01-23 16:23:25 -0300

    Enable distributed builds
    Fix path to ssh key

--- shared/nix-buildMachines-desk03.nix
+++ shared/nix-buildMachines-desk03.nix
@@ -1,16 +1,19 @@
-# Global configuration for Nix itself.
+# Configure desk03 as a nix remote builder
 { config, pkgs, inputs, ssot, machine, ... }:
 
 {
+  nix.distributedBuilds = true; # Required to use the builders
+
   nix.buildMachines = [
     {
       # ssot.desktop.hostname
       hostName = "192.168.1.70"; # TODO: this sucks. Make it better. VPN?
       sshUser = "h";
       protocol = "ssh"; # TODO: evaluate ssh-ng instead
-      sshKey = "~/.ssh/id_ed25519.${machine.hostname}";
+      sshKey = "/home/h/.ssh/id_ed25519.${machine.hostname}";
       systems = [ "x86_64-linux" ];
       maxJobs = 16;
+      speedFactor = 4;
       supportedFeatures = [ "big-parallel" ];
     }
   ];

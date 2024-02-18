5930313212f3258d8e629b0d469d2953e48ec795
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-02-18 18:07:26 -0300

    hosts:L14: Setup USB4/Thunderbolt

--- hosts/thinkpadL14/configuration.nix
+++ hosts/thinkpadL14/configuration.nix
@@ -22,6 +22,9 @@
   # TODO: add OpenCL support
   # TODO: add Vulkan support
 
+  # USB4 / Thunderbolt
+  services.hardware.bolt.enable = true;
+
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.

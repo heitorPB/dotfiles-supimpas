181d849108e62b69ec465f5c18d2fc8ea01ec3a5
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-03-10 16:00:51 -0300

    core: enable pdscd for gpg

--- hosts/core.nix
+++ hosts/core.nix
@@ -238,4 +238,7 @@
     enableSSHSupport = true; # Make GPG through SSH work
     pinentryFlavor = machine.gpgPinentry; # Options: curses, gnome3, gtk2, qt
   };
+  # Required to get pinentry working
+  # https://discourse.nixos.org/t/cant-get-gnupg-to-work-no-pinentry/15373/10
+  services.pcscd.enable = true;
 }

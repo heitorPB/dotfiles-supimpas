950f8225bef5e57bb75aa40d3c5f63da6bdda2f5
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-03-14 22:18:02 -0300

    examples: add flake.nix dev shell for terraform

---
+++ examples/flakes/terraform-packer/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake for Terraform and Packer development";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in
+      rec {
+        # For nix develop
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            export AWS_PROFILE="foo"
+          '';
+
+          nativeBuildInputs = with pkgs; [
+            terraform
+            terraform-lsp
+            packer
+          ];
+        };
+      });
+}

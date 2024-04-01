75752943b4704b9f40f4f1b4c65319787393bebe
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-04-01 10:17:54 -0300

    Add examples directories

---
+++ examples/flakes/python/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        # For nix develop
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            export PIP_NO_BINARY="ruff"
+          '';
+
+          nativeBuildInputs = with pkgs; [
+            ruff
+
+            (python311.withPackages (p: with p; [
+              python-lsp-server
+              python-lsp-ruff
+            ]))
+
+            rustc
+            cargo
+          ];
+        };
+      });
+}

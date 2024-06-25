062daa38312cd5d91178d47b8ccf4c43402c477e
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-06-25 06:09:50 -0300

    shared/power-saving-laptop: configure auto-cpufreq

--- shared/power-saving-laptop.nix
+++ shared/power-saving-laptop.nix
@@ -1,9 +1,30 @@
 # Power saving services for my laptop
-{ config, pkgs, inputs, ssot, machine, ... }:
+{ ... }:
 
 {
-  services.auto-cpufreq.enable = true;
   services.power-profiles-daemon.enable = false; # Replaced by tlp
-  services.tlp.enable = true;
   services.upower.enable = true;
+
+  services.tlp.enable = false;
+
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        governor = "powersave";
+        # Minimum CPU frequency when using battery, in kHz
+        scaling_min_freq = 800000;
+
+        # Experimental battery charging levels
+        # NOTE: not all laptops are supported, currently only Lenovo. See
+        # https://github.com/AdnanHodzic/auto-cpufreq/blob/v2.3.0/README.md#battery-charging-thresholds
+        enable_thresholds = true;
+        start_threshold = 20;
+        stop_threshold = 80;
+      };
+      charger = {
+        governor = "performance";
+      };
+    };
+  };
 }

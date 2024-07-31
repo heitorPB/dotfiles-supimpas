4705964b885cbd8daabe30d54a301e280f1b90ce
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-07-31 10:39:54 -0300

    examples: make nixpkgs input explicitly use GitHub

--- examples/flakes/python/flake.nix
+++ examples/flakes/python/flake.nix
@@ -2,12 +2,12 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
--- examples/flakes/terraform-packer/flake.nix
+++ examples/flakes/terraform-packer/flake.nix
@@ -2,12 +2,12 @@
   description = "Flake for Terraform and Packer development";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;

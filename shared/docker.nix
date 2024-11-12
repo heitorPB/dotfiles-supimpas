0c3c9d37d2773ba4c8dd9ebfb2fa60cf25cae04c
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2024-11-12 15:29:33 -0300

    shared/docker: configure default-address-pools
    Overwrite the default[0] address pools Dockerd uses for new networks. Make
    the new subnets be a /24 instead of a /16 or /20.
    
    See [1] for some extra discussion.
    
    - [0] https://docs.docker.com/engine/daemon/ipv6/#dynamic-ipv6-subnet-allocation
    - [1] https://www.reddit.com/r/selfhosted/comments/1az6mqa/psa_adjust_your_docker_defaultaddresspool_size/

--- shared/docker.nix
+++ shared/docker.nix
@@ -5,6 +5,21 @@
     enableOnBoot = false;
     #storageDriver = "zfs";
     autoPrune.enable = true;
+
+    daemon.settings = {
+      # Adjust network size
+      # https://www.reddit.com/r/selfhosted/comments/1az6mqa/psa_adjust_your_docker_defaultaddresspool_size/
+      # Each new Docker subnet will be a /24 (254 usable addresses) starting
+      # from a base subnet.
+      # See here for the default:
+      # https://docs.docker.com/engine/daemon/ipv6/#dynamic-ipv6-subnet-allocation
+      default-address-pools = [
+        {
+          base = "172.17.0.0/12";
+          size = 24;
+        }
+      ];
+    };
   };
 
   environment.systemPackages = with pkgs; [

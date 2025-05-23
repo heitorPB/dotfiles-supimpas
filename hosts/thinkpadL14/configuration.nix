1c3e33a34a694622d45c10d3a5dfc15e21216339
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2025-05-23 11:09:11 -0300

    hosts/L14: drop steam
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- hosts/thinkpadL14/configuration.nix
+++ hosts/thinkpadL14/configuration.nix
@@ -82,8 +82,6 @@
   # USB4 / Thunderbolt
   services.hardware.bolt.enable = true;
 
-  programs.steam.enable = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #

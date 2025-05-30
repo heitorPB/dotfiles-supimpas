78f345545c8a53daf6f2f80564680c0a0266d3cb
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2025-05-30 15:07:59 -0300

    home/nvim: indent shell scripts with 4 spaces

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -210,11 +210,12 @@
       autocmd BufNewFile,BufRead *csv set filetype=csv
 
       autocmd FileType csv setlocal textwidth=0       " disable line wraps for csv
+      autocmd FileType ledger setlocal ts=4 sts=4 sw=4 expandtab " use 4 spaces to indent ledger
       autocmd FileType markdown setlocal textwidth=79 " make text break lines at 79 chars
       autocmd FileType rst setlocal textwidth=79      " make text break lines at 79 chars
+      autocmd FileType sh setlocal ts=4 sts=4 sw=4 expandtab " use 4 spaces to indent shell scripts
       autocmd FileType terraform setlocal sw=2 expandtab " use 2 spaces as indentation
       autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab foldmethod=indent " use 2 spaces to indent yaml
-      autocmd FileType ledger setlocal ts=4 sts=4 sw=4 expandtab " use 4 spaces to indent ledger
 
       " Strip trailing whitespace (,ss)
       function! StripWhitespace()

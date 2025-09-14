86bd2dec3e87b50d24f5e0205c62041e0580ab02
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2025-09-14 18:22:03 -0300

    home-manager/nvim/zettel: drop `draft` from front_matter
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -67,7 +67,6 @@
       let front_matter = {}
       let front_matter.front_matter = {}
       let front_matter.front_matter.keywords = []
-      let front_matter.front_matter.draft = "false"
       let g:zettel_options = [front_matter]
       " change default new filename to date-title.md
       let g:zettel_format = "%title"

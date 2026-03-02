09a28b9e1ae6b2435d74800315931bdbf4d22286
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-03-02 17:38:13 -0300

    shared/power-saving-laptop: replace auto-cpufreq with TLP
    I never liked how my laptop behaved like a slug on battery, I want to
    experiment with TLP instead of auto-cpufreq for setting CPU governors,
    battery threshold levels, and CPU boost.
    
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- shared/power-saving-laptop.nix
+++ shared/power-saving-laptop.nix
@@ -3,26 +3,19 @@
   services.power-profiles-daemon.enable = false; # Replaced by tlp
   services.upower.enable = true;
 
-  services.tlp.enable = false;
-
-  services.auto-cpufreq = {
+  services.tlp = {
     enable = true;
     settings = {
-      battery = {
-        governor = "powersave";
-        # Minimum CPU frequency when using battery, in kHz
-        scaling_min_freq = 1000000;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        # Experimental battery charging levels
-        # NOTE: not all laptops are supported, currently only Lenovo. See
-        # https://github.com/AdnanHodzic/auto-cpufreq/blob/v2.3.0/README.md#battery-charging-thresholds
-        enable_thresholds = true;
-        start_threshold = 20;
-        stop_threshold = 80;
-      };
-      charger = {
-        governor = "performance";
-      };
+      # Allow CPU turbo-boost (Intel) / turbo core (AMD)
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 1;
+
+      # Help save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
     };
   };
 }

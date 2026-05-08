471ea9ebeaa4f1c3eba59cb896679a8eddd51e44
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-05-08 14:31:39 -0300

    shared/tailscale: add note about firewall opening
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- shared/tailscale.nix
+++ shared/tailscale.nix
@@ -1,3 +1,8 @@
+# Settings for Tailscale _clients_.
+# Note: this exposes all connections on tailscale0 interface. See
+# https://nixos.wiki/wiki/Tailscale#Tailscale_bypasses_the_firewall_for_all_incoming_traffic_on_tailscale0
+# for details. I will keep it until I figure out how to `tailscale serve
+# --set-path` programmatically
 {lib, ...}: {
   services.tailscale = {
     enable = true;

357991c536a50d46affd7b6fc5f637ab81e79d13
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-06-11 14:24:19 -0300

    shared/geolocation: fix European cities; add Campinas
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- shared/geolocation.nix
+++ shared/geolocation.nix
@@ -1,35 +1,43 @@
 {
   # https://m.xkcd.com/2170/
 
-  geneve = {
+  campinas = {
+    city = "Campinas";
+    country = "Brazil";
+    timezone = "America/Sao_Paulo";
+    latitude = "-22.92";
+    longitude = "-47.05";
+  };
+
+  cern = {
     city = "Geneve";
     country = "Switzerland";
     timezone = "Europe/Zurich";
-    latitude = "60.2";
-    longitude = "24.9";
+    latitude = "26.23";
+    longitude = "6.05";
   };
 
   helsinki = {
     city = "Helsinki";
     country = "Finland";
     timezone = "Europe/Finland";
-    latitude = "46.2339717";
-    longitude = "6.0557211";
+    latitude = "60.17";
+    longitude = "24.92";
   };
 
   piracicaba = {
     city = "Piracicaba";
     country = "Brazil";
     timezone = "America/Sao_Paulo";
-    latitude = "-22.7";
-    longitude = "-47.6";
+    latitude = "-22.74";
+    longitude = "-47.63";
   };
 
   saoCarlos = {
     city = "São Carlos";
     country = "Brazil";
     timezone = "America/Sao_Paulo";
-    latitude = "-22.0183737";
-    longitude = "-47.8911570";
+    latitude = "-22.01";
+    longitude = "-47.89";
   };
 }

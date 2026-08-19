79bcf0131d0c4bea84ea6640156ca963ec2fa3e1
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2026-08-19 10:38:49 -0300

    hosts/thinkpadL14: add zlib to nix-ld.libraries
    This is to run some Python packages (e.g. NumPy, psycopg2-binary)
    without having to change LD_LIBRARY_PATH via a devShell.
    
    Signed-off-by: Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>

--- hosts/thinkpadL14/configuration.nix
+++ hosts/thinkpadL14/configuration.nix
@@ -79,6 +79,10 @@
   # USB4 / Thunderbolt
   services.hardware.bolt.enable = true;
 
+  programs.nix-ld.libraries = [
+    pkgs.zlib # For NumPy, psycopg2-binary, etc
+  ];
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
